765c7f24c59a967aa3a9be56d1235cbe397bae7a
Natalie-Cardot <nataliecardot@gmail.com>
2021-01-17 19:23:22 -0800

    Add song data to DOM

--- script.js
+++ script.js
@@ -18,7 +18,40 @@ async function searchSongs(term) {
 }
 
 // Show song and artist in DOM
-function showData(data) {}
+function showData(data) {
+  // let output = '';
+
+  // data.data.forEach((song) => {
+  //   // Using HTML5 custom attributes "data-"
+  //   output += `
+  //     <li>
+  //       <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+  //       <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+  //     </li>
+  //   `;
+  // });
+
+  // result.innerHTML = `
+  //   <ul class="songs">
+  //     ${output}
+  //   </ul>
+  // `;
+
+  result.innerHTML = `
+    <ul class="songs">
+      ${data.data
+        .map(
+          (song) => `
+      <li>
+        <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+      </li>
+    `
+        )
+        .join('')}
+    </ul>
+  `;
+}
 
 // Event listeners
 form.addEventListener('submit', (e) => {
